bf9eb90ded4ce460cd91fb5ee052b07983b13a8d
Uhan3 <156217361+Uhan3@users.noreply.github.com>
2024-03-28 21:16:56 -0400

    Simplify MessageDataBase.java

--- MessageDataBase.java
+++ MessageDataBase.java
@@ -3,35 +3,21 @@ import java.util.HashMap;
 
 public class MessageDataBase {
 
-    private ArrayList<Message> sendArrayList = new ArrayList<>();
-    private ArrayList<Message> receiveArrayList = new ArrayList<>();
+    private ArrayList<Message> ArrayList = new ArrayList<>();
     private HashMap<String, ArrayList<Message>> messageHashMap = new HashMap<>();
 
-
-    public void addSendMessage(Message message) {
-        sendArrayList.add(message);
-        messageHashMap.put(message.getReceiver(), sendArrayList);
+    public void addMessage(Message message) {
+        ArrayList.add(message);
+        messageHashMap.put(message.getReceiver(), ArrayList);
     }
 
-    public void addReceiveMessage(Message message) {
-        receiveArrayList.add(message);
-        messageHashMap.put(message.getSender(), receiveArrayList);
+
+    public ArrayList<Message> getArrayList() {
+        return ArrayList;
     }
 
-    public ArrayList<Message> getSendArrayList() {
-        return sendArrayList;
-    }
-
-    public void setSendArrayList(ArrayList<Message> sendArrayList) {
-        this.sendArrayList = sendArrayList;
-    }
-
-    public ArrayList<Message> getReceiveArrayList() {
-        return receiveArrayList;
-    }
-
-    public void setReceiveArrayList(ArrayList<Message> receiveArrayList) {
-        this.receiveArrayList = receiveArrayList;
+    public void setArrayList(ArrayList<Message> sendArrayList) {
+        this.ArrayList = sendArrayList;
     }
 
     public HashMap<String, ArrayList<Message>> getMessageHashMap() {
