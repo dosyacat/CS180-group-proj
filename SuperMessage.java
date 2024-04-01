ddfed42309e31843012443c5b93b87c1671d8d21
Shresthi7524 <150051891+Shresthi7524@users.noreply.github.com>
2024-04-01 17:02:02 -0400

    SuperMessage.java

--- SuperMessage.java
+++ SuperMessage.java
@@ -1,3 +1,11 @@
+/**
+ * Interface SuperMessage
+ *
+ * <p>Purdue University -- CS18000 -- Spring 2024</p>
+ *
+ * @author Yuhan Zeng, Yeldos Zhumakyn, Shresthi Srivastava, Bryce Wong  , Kaustubh Mathur
+ * @version April 1, 2024
+ */
 public interface SuperMessage {
 
     String getSender();
