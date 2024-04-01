7beff1385bc0156f5a9e73125130eae0eb134f55
Shresthi7524 <150051891+Shresthi7524@users.noreply.github.com>
2024-04-01 13:04:31 -0400

    Add files via upload

---
+++ PictureInterface.java
@@ -0,0 +1,13 @@
+public interface PictureInterface {
+
+    // Uploads a picture from the user's computer.
+    public static byte[] uploadPicture() {
+        // Method signature, no implementation in interface
+        return null;
+    }
+
+    // Displays an image.
+    public static void displayPicture(byte[] imageData) {
+        // Method signature, no implementation in interface
+    }
+}
