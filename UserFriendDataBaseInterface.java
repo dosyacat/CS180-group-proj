fa3c277e04ac67970b8397580d8c854dfe902f1a
Shresthi7524 <150051891+Shresthi7524@users.noreply.github.com>
2024-04-01 16:59:35 -0400

    UserFriendDataBaseInterface.java

--- UserFriendDataBaseInterface.java
+++ UserFriendDataBaseInterface.java
@@ -1,4 +1,12 @@
 import java.util.HashMap;
+/**
+ * Interface for UserFriendDataBase.java
+ *
+ * <p>Purdue University -- CS18000 -- Spring 2024</p>
+ *
+ * @author Yuhan Zeng, Yeldos Zhumakyn, Shresthi Srivastava, Bryce Wong  , Kaustubh Mathur
+ * @version April 1, 2024
+ */
 
 public interface UserFriendDataBaseInterface {
 
