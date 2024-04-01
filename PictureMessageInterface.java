5a588ab51c3029b9833ec0150456e6ed57f60884
Shresthi7524 <150051891+Shresthi7524@users.noreply.github.com>
2024-04-01 13:12:41 -0400

    Add files via upload

---
+++ PictureMessageInterface.java
@@ -0,0 +1,31 @@
+public interface PictureMessageInterface {
+
+    // Gets the sender of the picture message.
+    public String getSender();
+
+    // Sets the sender of the picture message.
+    public void setSender(String sender);
+
+    // Gets the receiver of the picture message.
+    public String getReceiver();
+
+    // Sets the receiver of the picture message.
+    public void setReceiver(String receiver);
+
+    // Gets the image data of the picture message.
+    public byte[] getImage();
+
+    // Sets the image data of the picture message.
+    public void setImage(byte[] image);
+
+    // Gets the time of the picture message being sent.
+    public String getMessageTime();
+
+    // Sets the time of the picture message being sent.
+    public void setMessageTime(String messageTime);
+
+    // Returns a string representation of the picture message.
+    // Displays the picture.
+    @Override
+    public String toString();
+}
