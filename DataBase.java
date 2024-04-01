c14bdb9415b95dde17332973dd173d99f5f693a5
Shresthi7524 <150051891+Shresthi7524@users.noreply.github.com>
2024-04-01 01:57:39 -0400

    DataBase.java

--- DataBase.java
+++ DataBase.java
@@ -1,19 +1,23 @@
 import java.util.HashMap;
+/**
+ * The DataBase class manages the storage and retrieval of user data.
+ */
 
 public class DataBase{
+    // HashMap to store users with their usernames as keys
     private static HashMap<String, User> userHashMap = new HashMap<>();
-
+    //Adds a user to the database.
     public static void add(User user) {
         userHashMap.put(user.getUsername(), user);
     }
-
+    //Checks if the provided account and password match any user in the database.
     public static boolean check(String account, String password) {
         User user = userHashMap.get(account);
         if (user == null) return false;
         if (!(user.getPassword().equals(password))) return false;
         return true;
     }
-
+    //Displays information about all users in the database and allows viewing their profile pictures
     public static void userInformation() {
         System.out.println("Here is the information for our users!");
         int i = 1;
@@ -39,7 +43,7 @@ public class DataBase{
             DataBase.findUser(username).showProfilePicture();
         }
     }
-
+    //Finds a user in the database by username.
     public static User findUser(String username) {
         return userHashMap.get(username);
     }
