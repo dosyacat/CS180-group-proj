571fc0bbf7bdac140fad426eb3e9de97d2ce75df
Shresthi7524 <150051891+Shresthi7524@users.noreply.github.com>
2024-04-15 14:31:48 -0400

    InformationInterface.java

--- InformationInterface.java
+++ InformationInterface.java
@@ -1,4 +1,11 @@
 import java.util.concurrent.ConcurrentHashMap;
+/**
+ * Interface for Information Class
+ * <p>Purdue University -- CS18000 -- Spring 2024</p>
+ *
+ * @author Yuhan Zeng, Yeldos Zhumakyn, Shresthi Srivastava, Bryce Wong , Kaustubh Mathur
+ * @version April 15, 2024
+ */
 
 public interface InformationInterface {
     static void clearFile(String filePath) {}
