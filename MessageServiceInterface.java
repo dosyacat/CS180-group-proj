298fa884ddb26e6579ea1ccfa4a8fa286d86a416
Shresthi7524 <150051891+Shresthi7524@users.noreply.github.com>
2024-04-15 14:35:16 -0400

    MessageServiceInterface.java

--- MessageServiceInterface.java
+++ MessageServiceInterface.java
@@ -1,13 +1,15 @@
 import java.util.Date;
+/**
+ * Interface for MessageService Class
+ * <p>Purdue University -- CS18000 -- Spring 2024</p>
+ *
+ * @author Yuhan Zeng, Yeldos Zhumakyn, Shresthi Srivastava, Bryce Wong , Kaustubh Mathur
+ * @version April 15, 2024
+ */
 
 public interface MessageServiceInterface {
-
-    /* Sends a text message from the sender to the receiver with the given content.
-       sender   The username of the sender
-       receiver The username of the receiver
-       content  The content of the message */
+    
     public static void sendMessage(String sender, String receiver, String content) {
-        // Method signature, no implementation in interface
     }
 
 }
