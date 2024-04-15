727c0c32aa6ab21463e69bbffae59d624c899cd2
Shresthi7524 <150051891+Shresthi7524@users.noreply.github.com>
2024-04-15 14:39:51 -0400

    UserMessageDataBaseInterface.java

--- UserMessageDataBaseInterface.java
+++ UserMessageDataBaseInterface.java
@@ -1,5 +1,12 @@
 import java.util.ArrayList;
 import java.util.HashMap;
+/**
+ * Interface for UserMessageDataBase Class
+ * <p>Purdue University -- CS18000 -- Spring 2024</p>
+ *
+ * @author Yuhan Zeng, Yeldos Zhumakyn, Shresthi Srivastava, Bryce Wong , Kaustubh Mathur
+ * @version April 15, 2024
+ */
 
 public interface UserMessageDataBaseInterface {
 
