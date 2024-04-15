7ddc6d42cadd756d3a60a6835c034d0a666055f4
Shresthi7524 <150051891+Shresthi7524@users.noreply.github.com>
2024-04-15 14:34:26 -0400

    MessageInterface.java

--- MessageInterface.java
+++ MessageInterface.java
@@ -1,4 +1,11 @@
 import java.io.Serializable;
+/**
+ * Interface for Message Class
+ * <p>Purdue University -- CS18000 -- Spring 2024</p>
+ *
+ * @author Yuhan Zeng, Yeldos Zhumakyn, Shresthi Srivastava, Bryce Wong , Kaustubh Mathur
+ * @version April 15, 2024
+ */
 
 public interface MessageInterface extends Serializable {
 
