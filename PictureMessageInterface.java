813670b5f3b32353fddf1f1a4b5c6195be71df64
Shresthi7524 <150051891+Shresthi7524@users.noreply.github.com>
2024-04-15 14:36:36 -0400

    PictureMessageInterface.java

--- PictureMessageInterface.java
+++ PictureMessageInterface.java
@@ -1,3 +1,11 @@
+/**
+ * Interface for PictureMessage Class
+ * <p>Purdue University -- CS18000 -- Spring 2024</p>
+ *
+ * @author Yuhan Zeng, Yeldos Zhumakyn, Shresthi Srivastava, Bryce Wong , Kaustubh Mathur
+ * @version April 15, 2024
+ */
+
 public interface PictureMessageInterface {
 
     // Gets the sender of the picture message.
