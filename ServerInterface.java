2d4e078fffc28416504afb301f6faa4d3d94b8de
Shresthi7524 <150051891+Shresthi7524@users.noreply.github.com>
2024-04-15 14:13:00 -0400

    Add files via upload

---
+++ ServerInterface.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+public interface ServerInterface {
+    public void run();
+}
+
