298b9407daadf5bc6da3d0aede452b93057254c5
Shresthi7524 <150051891+Shresthi7524@users.noreply.github.com>
2024-04-15 13:29:30 -0400

    Picture.java

--- Picture.java
+++ Picture.java
@@ -12,7 +12,7 @@ import java.io.IOException;
  * The Picture class provides methods for uploading and displaying images.
  * <p>Purdue University -- CS18000 -- Spring 2024</p>
  * @author Yuhan Zeng, Yeldos Zhumakyn, Shresthi Srivastava, Bryce Wong  , Kaustubh Mathur
- * @version April 1, 2024
+ * @version April 15, 2024
  */
 
 public class Picture implements Serializable {
