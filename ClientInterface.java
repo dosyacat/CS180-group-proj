21e4c7bb304c1ddad698bd8a30a78dcd64f83011
Shresthi7524 <150051891+Shresthi7524@users.noreply.github.com>
2024-04-15 14:30:14 -0400

    ClientInterface.java

--- ClientInterface.java
+++ ClientInterface.java
@@ -1,3 +1,11 @@
+/**
+ * Interface for Client Class
+ * <p>Purdue University -- CS18000 -- Spring 2024</p>
+ *
+ * @author Yuhan Zeng, Yeldos Zhumakyn, Shresthi Srivastava, Bryce Wong , Kaustubh Mathur
+ * @version April 15, 2024
+ */
+ 
 public interface ClientInterface
 {
 
