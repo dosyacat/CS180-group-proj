1d039b1b4705b1588b86796251e453b3f000e17f
Shresthi7524 <150051891+Shresthi7524@users.noreply.github.com>
2024-04-15 14:38:33 -0400

    ServerInterface.java

--- ServerInterface.java
+++ ServerInterface.java
@@ -4,6 +4,13 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+/**
+ * Interface for Server Class
+ * <p>Purdue University -- CS18000 -- Spring 2024</p>
+ *
+ * @author Yuhan Zeng, Yeldos Zhumakyn, Shresthi Srivastava, Bryce Wong , Kaustubh Mathur
+ * @version April 15, 2024
+ */
 public interface ServerInterface {
     public void run();
 }
