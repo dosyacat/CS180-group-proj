0a549c3563836f3f923e6c57556b2f6271fc4f30
Shresthi7524 <150051891+Shresthi7524@users.noreply.github.com>
2024-04-15 13:46:57 -0400

    ClientConnectServerThread.java

--- ClientConnectServerThread.java
+++ ClientConnectServerThread.java
@@ -4,6 +4,14 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.HashMap;
+/**
+ * The ClientConnectServerThread class represents a thread that connects the client to the server.
+ * It handles communication between the client and the server.
+ * <p>Purdue University -- CS18000 -- Spring 2024</p>
+ * @author Yuhan Zeng, Yeldos Zhumakyn, Shresthi Srivastava, Bryce Wong  , Kaustubh Mathur
+ * @version April 15, 2024
+ */
+
 
 public class ClientConnectServerThread extends Thread {
     private Socket socket;
