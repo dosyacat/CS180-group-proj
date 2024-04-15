c6e1ca9c47f416e5ed2104dd37c052c58a82c982
Shresthi7524 <150051891+Shresthi7524@users.noreply.github.com>
2024-04-15 14:40:10 -0400

    UserServiceInterface.java

--- UserServiceInterface.java
+++ UserServiceInterface.java
@@ -1,5 +1,12 @@
 public interface UserServiceInterface
 {
+    /**
+ * Interface for UserService Class
+ * <p>Purdue University -- CS18000 -- Spring 2024</p>
+ *
+ * @author Yuhan Zeng, Yeldos Zhumakyn, Shresthi Srivastava, Bryce Wong , Kaustubh Mathur
+ * @version April 15, 2024
+ */
     public User userSignIn(String account, String password);
     public void userSignUp();
     public void userView();
