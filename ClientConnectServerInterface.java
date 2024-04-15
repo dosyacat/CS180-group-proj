d219cfec45a83bc1ddd976472c79986aa4fdb727
Shresthi7524 <150051891+Shresthi7524@users.noreply.github.com>
2024-04-15 14:30:45 -0400

    ClientConnectServerInterface.java

--- ClientConnectServerInterface.java
+++ ClientConnectServerInterface.java
@@ -1,7 +1,10 @@
 import java.net.Socket;
-
 /**
+ * Interface for ClientConnectServerThread Class
+ * <p>Purdue University -- CS18000 -- Spring 2024</p>
  *
+ * @author Yuhan Zeng, Yeldos Zhumakyn, Shresthi Srivastava, Bryce Wong , Kaustubh Mathur
+ * @version April 15, 2024
  */
 
 public interface ClientConnectServerInterface {
