d77f65ce5e70f2d3762b2669d61a92968045476d
Shresthi7524 <150051891+Shresthi7524@users.noreply.github.com>
2024-04-29 13:55:01 -0400

    LoginInterface.java

--- LoginInterface.java
+++ LoginInterface.java
@@ -1,3 +1,10 @@
+/**
+ * Interface for LoginMenu CLass
+ * <p>Purdue University -- CS18000 -- Spring 2024</p>
+ *
+ * @author Yuhan Zeng, Yeldos Zhumakyn, Shresthi Srivastava, Bryce Wong , Kaustubh Mathur
+ * @version April 29, 2024
+ */
 public interface LoginInterface
   {
   }
