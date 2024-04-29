717d0352bd7b808d264f389cc55319918ce3f4da
Shresthi7524 <150051891+Shresthi7524@users.noreply.github.com>
2024-04-29 14:00:46 -0400

    MessagesMenuInterface.java

--- MessagesMenuInterface.java
+++ MessagesMenuInterface.java
@@ -1,3 +1,9 @@
-public interface MessagesMenuInterface
+/**
+ * Interface for MesssageMenu CLass
+ * <p>Purdue University -- CS18000 -- Spring 2024</p>
+ *
+ * @author Yuhan Zeng, Yeldos Zhumakyn, Shresthi Srivastava, Bryce Wong , Kaustubh Mathur
+ * @version April 29, 2024
+ */public interface MessagesMenuInterface
   {
   }
