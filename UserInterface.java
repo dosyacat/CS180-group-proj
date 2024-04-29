5c74c3b600c9a1a035873801f1c96ef399cf3a1d
Shresthi7524 <150051891+Shresthi7524@users.noreply.github.com>
2024-04-29 12:36:20 -0400

    UserInterface.java

--- UserInterface.java
+++ UserInterface.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
  * <p>Purdue University -- CS18000 -- Spring 2024</p>
  *
  * @author Yuhan Zeng, Yeldos Zhumakyn, Shresthi Srivastava, Bryce Wong , Kaustubh Mathur
- * @version April 15, 2024
+ * @version April 29, 2024
  */
 
 public interface UserInterface extends Serializable {
