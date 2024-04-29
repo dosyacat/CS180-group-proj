1c97c75f1d79d571f46d9e9d0589500113824fb9
Shresthi7524 <150051891+Shresthi7524@users.noreply.github.com>
2024-04-29 13:50:14 -0400

    EditProfileInterface.java

--- EditProfileInterface.java
+++ EditProfileInterface.java
@@ -1,3 +1,10 @@
+/**
+ *  Interface for Edit Profile class
+ * <p>Purdue University -- CS18000 -- Spring 2024</p>
+ *
+ * @author Yuhan Zeng, Yeldos Zhumakyn, Shresthi Srivastava, Bryce Wong , Kaustubh Mathur
+ * @version April 29, 2024
+ */
 public interface EditProfileInterface
 {
 
